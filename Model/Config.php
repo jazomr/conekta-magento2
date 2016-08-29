929320d8607d7f4fd42f70b65bb52b1ee9292998
System Administrator <root@MacBook-Air-de-Cristian.local>
2016-08-29 13:25:25 -0500

    Webhook mode creation

--- Model/Config.php
+++ Model/Config.php
@@ -7,13 +7,14 @@ use Conekta\Webhook;
 use Conekta\Error;
 
 class Config extends \Magento\Payment\Model\Method\AbstractMethod {
+
     const CODE = 'conekta_config';
 
     protected $_code = self::CODE;
 
     public function createWebhook() {
         $sandbox_mode = (boolean) ((integer) $this->getConfigData("sandbox_mode"));
-        
+
         if ($sandbox_mode) {
             $privateKey = (string) $this->getConfigData("test_private_api_key");
         } else {
@@ -35,27 +36,38 @@ class Config extends \Magento\Payment\Model\Method\AbstractMethod {
             $url_webhook = $url->getUrl();
             unset($url);
         }
-        
+
         $events = ["events" => ["charge.paid"]];
         $error_message = null;
 
         try {
             $different = true;
             $webhooks = Webhook::where();
-            
-            foreach ($webhooks as $webhook){
+
+            foreach ($webhooks as $webhook) {
                 if (strpos($webhook->webhook_url, $url_webhook) !== false) {
                     $different = false;
                 }
             }
-            
-            if ($different){
-                $webhook = Webhook::create(array_merge(["url" => $url_webhook], $events));
+
+            if ($different) {
+                if (!$sandbox_mode) {
+                    $mode = array(
+                        "production_enabled" => 1
+                    );
+                } else {
+                    $mode = array(
+                        "development_enabled" => 1
+                    );
+                }
+
+                $webhook = Webhook::create(array_merge(["url" => $url_webhook], $mode, $events));
             }
         } catch (Error $e) {
             $error_message = $e->getMessage();
             $this->_logger->error(__('[Conekta]: Webhook error, Message: ' . $error_message . ' URL: ' . $url_webhook));
             throw new \Magento\Framework\Validator\Exception(__('Can not register this webhook ' . $url_webhook . '<br>' . 'Message: ' . (string) $error_message));
-        }   
+        }
     }
+
 }
